702a69f800ce3a119db007b72ec4f0dfdeca9353
Danielrenquin <danielrenquin2@gmail.com>
2025-01-15 11:15:41 +0100

    execute_cmd splited

--- src/execute_cmd.c
+++ src/execute_cmd.c
@@ -12,7 +12,7 @@
 
 #include "../include/minishell.h"
 
-void	execute_command1(t_shell *shell)
+/*void	execute_command1(t_shell *shell)
 {
 	t_list	*tokens;
 	int		has_pipe;
@@ -44,4 +44,39 @@ void	execute_command1(t_shell *shell)
 			free_cmd(cmd);
 		}
 	}
+}*/
+int     check_for_pipe(t_list *tokens)
+{
+    char    *token;
+
+    while (tokens)
+    {
+        token = tokens->content;
+        if (ft_strcmp(token, "|") == 0)
+            return (1);
+        tokens = tokens->next;
+    }
+    return (0);
 }
+
+void    execute_command1(t_shell *shell)
+{
+    t_cmd   *cmd;
+    int     has_pipe;
+
+    if (shell->tokens == NULL)
+        return ;
+    has_pipe = check_for_pipe(shell->tokens);
+    if (has_pipe)
+        execute_pipeline(shell);
+    else
+    {
+        cmd = parse_command(shell->tokens);
+        if (cmd)
+        {
+            execute_simple_command(cmd, shell);
+            free_cmd(cmd);
+        }
+    }
+}
+
